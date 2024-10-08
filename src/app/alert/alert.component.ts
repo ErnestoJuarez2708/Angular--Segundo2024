0add3d076adf3d07c0d95fa280a734fc9697133d
Ernesto Juarez <ernestojuarezgutierrez03@gmail.com>
2024-10-07 20:21:44 -0400

    [07-10-2024] shared-nodule, alert, card, ng-styles, navbar

---
+++ src/app/alert/alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alert',
+  templateUrl: './alet.component.html',
+  styleUrl: './alet.component.scss'
+})
+export class AlertComponent {
+
+}
