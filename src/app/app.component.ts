1474bf69efa11055798c76307c3686c611dc2b32
Ernesto Juarez <ernestojuarezgutierrez03@gmail.com>
2024-10-22 20:53:16 -0400

    [22-10-2024] template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { PurePipe } from "./pure.pipe";
 import { ImpurePipe } from "./impure.pipe";
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from "@angular/material/button";
+import { FormsModule } from "@angular/forms";
 
 interface Person {
   name: string;
@@ -35,12 +36,18 @@ interface Person {
     PurePipe,
     ImpurePipe,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule
   ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
+  name:string='testname'
+  lastname:string=''
+
+
+
   users = [
     { name: "abc", email: "abs@gmail.com" },
     { name: "dfg", email: "dfg@gmail.com" },
@@ -184,5 +191,9 @@ export class AppComponent {
 
   public onCalculator(){
     this.router.navigate(['cal'], {queryParams: {name: 'John', age: 20}})
+  }
+
+  onSubmit(data: any){
+    console.log("TEMPLATE DRIVEN FORM: ",data)
   }
 }
