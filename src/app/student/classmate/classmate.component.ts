711da46b171977513513b59fafdd8d0c24e910a7
Ernesto Juarez <ernestojuarezgutierrez03@gmail.com>
2024-10-24 20:46:11 -0400

    [24-10-2024] simple service

--- src/app/student/classmate/classmate.component.ts
+++ src/app/student/classmate/classmate.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ExamService } from '../exam.service';
 
 @Component({
   selector: 'app-classmate',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './classmate.component.scss'
 })
 export class ClassmateComponent {
+
+  newScore: number[] = []
+
+  constructor(private _examService: ExamService){
+    this._examService.getScoresAsObservable().subscribe(score => {
+      console.log('SCORES: ', score)
+      this.newScore = score
+    })
+  }
 
 }
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -4,6 +4,7 @@ import { StudentRoutingModule } from './student-routing.module';
 import { ScoreComponent } from './score/score.component';
 import { ClassmateComponent } from './classmate/classmate.component';
 import { AverageComponent } from './average/average.component';
+import { ExamService } from './exam.service';
 
 
 @NgModule({
@@ -15,6 +16,9 @@ import { AverageComponent } from './average/average.component';
   imports: [
     CommonModule,
     StudentRoutingModule
+  ],
+  providers: [
+    ExamService
   ]
 })
 export class StudentModule { }
